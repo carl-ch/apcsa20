01764dfbf485d2927ef70194c60b3168e665b817
chengc7444 <chengc7444@CA-SU-F106-10.sduhsd.lan>
2020-03-11 11:58:38 -0700

    almost done

--- u10+/src/celebLab/Celebrity.java
+++ u10+/src/celebLab/Celebrity.java
@@ -1,75 +1,75 @@
 package celebLab;
+
 /**
  * Celebrity base class for the Celebrity game.
+ * 
  * @author cody.henrichsen
  * @version 1.4 17/09/2018
  */
-public class Celebrity
-{
+public class Celebrity {
 	/**
 	 * The clue to determine the celebrity
 	 */
 	private String clue;
-	
+
 	/**
 	 * The answer or name of the celebrity.
 	 */
 	private String answer;
-	
+
 	/**
 	 * Creates a Celebrity instance with the supplied answer and clue
+	 * 
 	 * @param answer
 	 * @param clue
 	 */
-	public Celebrity(String ans, String clu)
-	{
+	public Celebrity(String ans, String clu) {
 		setClue(clu);
 		setAnswer(ans);
 	}
 
 	/**
 	 * Supplies the clue for the celebrity
+	 * 
 	 * @return
 	 */
-	public String getClue()
-	{
+	public String getClue() {
 		return clue;
 	}
 
 	/**
 	 * Supplies the answer for the celebrity.
+	 * 
 	 * @return
 	 */
-	public String getAnswer()
-	{
+	public String getAnswer() {
 		return answer;
 	}
 
 	/**
 	 * Updates the clue to the provided String.
+	 * 
 	 * @param clue The new clue.
 	 */
-	public void setClue(String clu)
-	{
+	public void setClue(String clu) {
 		clue = clu;
 	}
 
 	/**
 	 * Updates the answer to the provided String.
+	 * 
 	 * @param answer The new answer.
 	 */
-	public void setAnswer(String ans)
-	{
+	public void setAnswer(String ans) {
 		answer = ans;
 	}
-	
+
 	/**
 	 * Provides a String representation of the Celebrity.
 	 */
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "hint: " + clue + ", name: " + answer + "\n";
 	}
-	
+
 }
