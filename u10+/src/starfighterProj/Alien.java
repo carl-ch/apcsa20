024011a34f043fc0b59ca1ae09bfff94b73eb655
canna <canna@DESKTOP-L4H1HJE>
2020-05-25 04:41:19 -0700

    finish bullets and horde

--- u10+/src/starfighterProj/Alien.java
+++ u10+/src/starfighterProj/Alien.java
@@ -1,5 +1,6 @@
 package starfighterProj;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
@@ -10,72 +11,60 @@ import java.awt.Graphics;
 import java.awt.Image;
 import javax.imageio.ImageIO;
 
-public class Alien extends MovingThing
-{
+public class Alien extends MovingThing {
 	private int speed;
 	private Image image;
+	
 
-	public Alien()
-	{
-		this(0,0,30,30,0);
+	public Alien() {
+		this(0, 0, 30, 30, 0);
 	}
 
-	public Alien(int x, int y)
-	{
-		super(x,y);
+	public Alien(int x, int y) {
+		super(x, y);
 	}
 
-	public Alien(int x, int y, int s)
-	{
-		super(x,y);
+	public Alien(int x, int y, int s) {
+		super(x, y);
 		speed = s;
 	}
 
-	public Alien(int x, int y, int w, int h, int s)
-	{
-		super(x, y, w,h);
-		speed=s;
-		try
-		{
+	public Alien(int x, int y, int w, int h, int s) {
+		super(x, y, w, h);
+		speed = s;
+		try {
 			URL url = getClass().getResource("/starfighterProj/alien.jpg");
 			image = ImageIO.read(url);
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("no img");
 		}
 	}
 
-	public void setSpeed(int s)
-	{
-	   speed = s;
+	public void setSpeed(int s) {
+		speed = s;
 	}
 
-	public int getSpeed()
-	{
-	   return speed;
+	public int getSpeed() {
+		return speed;
 	}
 
-   public void move(String direction)
-	{
-		if(direction.equals("LEFT")) {
-			super.setX(super.getX()-speed);
-		}
-		else if(direction.equals("RIGHT")) {
-			super.setX(super.getX()+speed);
-		}
-		else if(direction.equals("DOWN")) {
-			super.setY(super.getY()+speed);
+	public void move(String direction) {
+		if (direction.equals("LEFT")) {
+			super.setX(super.getX() - speed);
+		} 
+		else if (direction.equals("RIGHT")) {
+			super.setX(super.getX() + speed);
+		} 
+		else if (direction.equals("DOWN")) {
+			super.setY(super.getY() + speed);
 		}
 	}
 
-	public void draw( Graphics window )
-	{
-   	window.drawImage(image,getX(),getY(),getWidth(),getHeight(),null);
+	public void draw(Graphics window) {
+		window.drawImage(image, getX(), getY(), getWidth(), getHeight(), null);
 	}
 
-	public String toString()
-	{
+	public String toString() {
 		return "";
 	}
 }
--- u10+/src/starfighterProj/Ammo.java
+++ u10+/src/starfighterProj/Ammo.java
@@ -9,48 +9,45 @@ import java.awt.Image;
 import java.io.File;
 import javax.imageio.ImageIO;
 
-public class Ammo extends MovingThing
-{
+public class Ammo extends MovingThing {
 	private int speed;
 
-	public Ammo()
-	{
-		this(0,0,0);
+	public Ammo() {
+		this(0, 0, 0);
 	}
 
-	public Ammo(int x, int y)
-	{
-		//add code
+	public Ammo(int x, int y) {
+		super(x, y);
 	}
 
-	public Ammo(int x, int y, int s)
-	{
-		//add code
+	public Ammo(int x, int y, int s) {
+		super(x, y);
+		speed = s;
 	}
 
-	public void setSpeed(int s)
-	{
-	   //add code
+	public void setSpeed(int s) {
+		speed = s;
 	}
 
-	public int getSpeed()
-	{
-	   return 0;
+	public int getSpeed() {
+		return speed;
 	}
 
-	public void draw( Graphics window )
-	{
-		//add code to draw the ammo
-	}
-	
-	
-	public void move( String direction )
-	{
-		//add code to draw the ammo
+	public void draw(Graphics window) {
+		window.drawRect(super.getX(), super.getY(), 4, 1);
 	}
 
-	public String toString()
-	{
+	public void move(String direction) {
+		if (direction.equals("UP")) {
+			super.setY(super.getY() - speed);
+		}
+		
+		else if (direction.equals("DOWN")) {
+			super.setY(super.getY() + speed);
+		}
+	}
+
+	public String toString() {
 		return "";
 	}
 }
